828a516751206bfb37478621677f2c2b6623de81
Cooper Luce <16cluce@gmail.com>
2023-11-18 17:00:47 -0500

    created a var to store the config file

--- init.el
+++ init.el
@@ -6,4 +6,22 @@
 ;; `org-tangle' when there aren't any source blocks.
 ;; TODO: handle this error. probably send a different error which more
 ;;       explicitly says what is _probably_ going on.
-(org-babel-load-file "config.org")
+(defconst user-config-file
+  (locate-user-emacs-file "config.org")
+  "
+Absolute path to the user's `config.org' file if it exists.
+Otherwise, just yield the init file.")
+
+(org-babel-load-file user-config-file)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(magit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
